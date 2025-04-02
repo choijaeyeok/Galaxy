00d5176fd0eeaf9dc2b771c63ccb812e7e20dc28
choijaeyeok <52644877+choijaeyeok@users.noreply.github.com>
2025-04-02 22:23:28 +0900

    0402

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.Controls.AxisControl;
@@ -7,16 +8,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float controlSpeed = 10f;
     [SerializeField] float xClampRange = 5f;
     [SerializeField] float yClampRange = 5f;
+
+    [SerializeField] float controlPitchFactor = 10f;
+    [SerializeField] float controlRollFactor = 20f;
+    [SerializeField] float rotationSpeed = 10f;
     Vector2 movement;//movement는 이동 방향을 저장(플레이어가 어디로 갈지 알려주는 값)
     void Update()
     {
         ProcessTranslation();
+        ProcessRotation();
     }                                                                                                                                                        
     public void OnMove(InputValue value) //유니티의 새로운 Input System에서 사용되는 입력 처리 함수, OnMove는 플레이어가 키보드나 조이스틱을 눌렀을 때 반응하는 함수
     {                                                                      //InputValue value는 플레이어가 입력한 방향 정보(WASD, 방향키, 조이스틱 등)가 저장
         movement = value.Get<Vector2>();//입력 값을 Vector2 형식으로 가져옴 /예)키보드 W를 누르면 (0, 1), A를 누르면 (-1, 0) 같은 값이 나옴
     }
-    private void ProcessTranslation()
+    void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;//movement.x는 수평 방향 (AD) 값을 나타냄, movement.x를 곱하는 이유는 이동 방향을 계산하기 위해서 -1이면 왼쪽 1이면 오른쪽
         float rawXPos = transform.localPosition.x + xOffset;//transform.localPosition.x는 지금 플레이어가 있는 현재 x 위치, xOffset는 키보드를 눌러서 이동할 거리
@@ -28,9 +34,15 @@ public class PlayerMovement : MonoBehaviour
         float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
         transform.localPosition = new Vector3 (clampedXPos, clampedYPos, 0f);//localPosition은 게임 오브젝트의 부모 객체를 기준으로 한 상대적인 위치를 설정하는 프로퍼티
     }                                                                                                                                                                                                          //현재 x 위치에 xOffset을 더해서 새로운 위치를 계산, 오른쪽으로 1만큼 가려면 transform.localPosition.x + 1이 되고,
+
+    void ProcessRotation()
+    {
+        float pitch = controlPitchFactor * movement.y;
+        float roll = -controlRollFactor * movement.x;
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);//Quaternion.Lerp(시작 회전, 목표 회전, 부드럽게 변하는 속도);
+    }
 }                                                                                                                                                                                                                   // 왼쪽으로 1만큼 가려면 transform.localPosition.x - 1이됨.
-
-
 
 
 
