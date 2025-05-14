3154f1d30894d64dcc5f2761d4b21821cc5e6cbc
choijaeyeok <52644877+choijaeyeok@users.noreply.github.com>
2025-05-14 23:47:29 +0900

    0514

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -13,6 +13,7 @@ public class CollisionHandler : MonoBehaviour
     {
         gameSceneManager.ReloadLevel();
         Instantiate(destroyedVFX, transform.position, Quaternion.identity);// Quaternion.identity는 회전 없음(0,0,0) 을 나타냄
+                                                                           // destroyedVFX라는 이펙트를, 이 오브젝트의 위치에서, 회전 없이 새로 생성한다
         Destroy(this.gameObject);
     }
 }
